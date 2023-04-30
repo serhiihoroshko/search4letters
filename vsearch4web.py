f57979d43dd5e1320eaec1f1e97b43cb3a5e47c3
Serhii Horoshko <serhiihoroshko@Serhiis-MacBook-Pro.local>
2023-04-30 13:52:23 +0300

    Added request with methods post and render templets with static styles, validate form for taking respons

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from vsearch import search4letters
 
 app = Flask(__name__)
@@ -7,8 +7,14 @@ app = Flask(__name__)
 def hello() -> str:
     return 'Hello world from Flask!'
 
-@app.route('/search4')
+@app.route('/search4', methods=['POST'])
 def do_search() -> str:
-    return str(search4letters('...life, the universe, and everything!'))
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    return str(search4letters(phrase, letters))
 
-app.run()
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html', the_title='Welcome to search4letters on the web!')
+
+app.run(debug=True)
