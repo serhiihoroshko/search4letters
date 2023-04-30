8f2bf1fd868c31919e55b0019e4e60b77facfd36
Serhii Horoshko <serhiihoroshko@Serhiis-MacBook-Pro.local>
2023-04-30 14:33:29 +0300

    Added results page with return renders templates

--- vsearch4web.py
+++ vsearch4web.py
@@ -8,10 +8,16 @@ def hello() -> str:
     return 'Hello world from Flask!'
 
 @app.route('/search4', methods=['POST'])
-def do_search() -> str:
+def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
-    return str(search4letters(phrase, letters))
+    title = 'Here are your result:'
+    results = str(search4letters(phrase, letters))
+    return render_template('results.html',
+                    the_phrase=phrase,
+                    the_letters=letters,
+                    the_title=title,
+                    the_results=results,)
 
 @app.route('/entry')
 def entry_page() -> 'html':
