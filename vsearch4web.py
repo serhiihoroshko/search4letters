cdae0aca04b1d5050f00d62230f3f7bc7d30a8b7
Serhii Horoshko <sr.horoshko@gmail.com>
2023-05-02 13:58:01 +0300

    Added logger logic with route for better reflection log file

--- vsearch4web.py
+++ vsearch4web.py
@@ -3,8 +3,12 @@ from vsearch import search4letters
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/entry')
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req, res, file=log)
+
+@app.route('/', methods=['GET'])
+@app.route('/entry', methods=['GET'])
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to search4letters on the web!')
 
@@ -14,10 +18,18 @@ def do_search() -> 'html':
     letters = request.form['letters']
     title = 'Here are your result:'
     results = str(search4letters(phrase, letters))
+    log_request(request, results)
     return render_template('results.html',
                     the_phrase=phrase,
                     the_letters=letters,
                     the_title=title,
                     the_results=results,)
 
-app.run(debug=True)
+@app.route('/viewlog', methods=['GET'])
+def view_the_log() -> str:
+    with open('vsearch.log') as log:
+        contents = log.read()
+    return contents
+
+if __name__ == '__main__':
+    app.run(debug=True)
