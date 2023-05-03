be1286b0892cb7310409fece17ca066c388db51e
Serhii Horoshko <sr.horoshko@gmail.com>
2023-05-03 12:54:46 +0300

    Added registration of the additional attribute for the logger function

--- vsearch.py
+++ vsearch.py
@@ -2,6 +2,7 @@ def search4vowels(phrase: str) -> set:
     """Returns the set of vowels found in 'phrase'."""
     return set('aeiou').intersection(set(phrase))
 
-def search4letters(phrase: str, letters: str='aeiou') -> set:
+
+def search4letters(phrase: str, letters: str = 'aeiou') -> set:
     """Returns the set of 'letters' found in 'phrase'."""
     return set(letters).intersection(set(phrase))
--- vsearch4web.py
+++ vsearch4web.py
@@ -1,16 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from vsearch import search4letters
 
 app = Flask(__name__)
 
+
 def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearch.log', 'a') as log:
-        print(req, res, file=log)
+        print(
+            req.form,
+            req.remote_addr,
+            req.user_agent,
+            res,
+            file=log,
+            end='|'
+        )
+
 
 @app.route('/', methods=['GET'])
 @app.route('/entry', methods=['GET'])
 def entry_page() -> 'html':
-    return render_template('entry.html', the_title='Welcome to search4letters on the web!')
+    return render_template(
+        'entry.html',
+        the_title='Welcome to search4letters on the web!'
+    )
+
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
@@ -20,16 +33,18 @@ def do_search() -> 'html':
     results = str(search4letters(phrase, letters))
     log_request(request, results)
     return render_template('results.html',
-                    the_phrase=phrase,
-                    the_letters=letters,
-                    the_title=title,
-                    the_results=results,)
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_title=title,
+                           the_results=results,)
+
 
 @app.route('/viewlog', methods=['GET'])
 def view_the_log() -> str:
     with open('vsearch.log') as log:
         contents = log.read()
-    return contents
+    return escape(contents)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
