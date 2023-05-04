3e839454640adba3f9c067cf946a74dc6d0a1bbd
Serhii Horoshko <sr.horoshko@gmail.com>
2023-05-04 13:53:24 +0300

    added viewlog.html, changed log output behavior is now a list of lists, changes document format base on pep8, changes document format in all html files

--- vsearch4web.py
+++ vsearch4web.py
@@ -6,30 +6,26 @@ app = Flask(__name__)
 
 def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearch.log', 'a') as log:
-        print(
-            req.form,
-            req.remote_addr,
-            req.user_agent,
-            res,
-            file=log,
-            end='|'
-        )
+        print(req.form,
+              req.remote_addr,
+              req.user_agent,
+              res,
+              file=log,
+              sep='|')
 
 
 @app.route('/', methods=['GET'])
 @app.route('/entry', methods=['GET'])
 def entry_page() -> 'html':
-    return render_template(
-        'entry.html',
-        the_title='Welcome to search4letters on the web!'
-    )
+    return render_template('entry.html',
+                           the_title='Welcome to search4letters on the web!')
 
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
-    title = 'Here are your result:'
+    title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
     log_request(request, results)
     return render_template('results.html',
@@ -40,10 +36,18 @@ def do_search() -> 'html':
 
 
 @app.route('/viewlog', methods=['GET'])
-def view_the_log() -> str:
+def view_the_log() -> 'html':
+    contents = []
     with open('vsearch.log') as log:
-        contents = log.read()
-    return escape(contents)
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                           the_title='View Log',
+                           the_row_titles=titles,
+                           the_data=contents,)
 
 
 if __name__ == '__main__':
