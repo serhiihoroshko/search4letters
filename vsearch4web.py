528f0a1af52b0bc37330bc8922bee93aa0b4fbb6
Serhii Horoshko <sr.horoshko@gmail.com>
2023-06-10 19:45:35 +0300

    Major: Added connection for SQL DB, create db context manager module, view logs taken from DB

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,17 +1,26 @@
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
+from DatabaseContextManager import UserDatabase
 
 app = Flask(__name__)
 
+app.config['dbconfig'] = {'host': '127.0.0.1',
+                          'user': 'vsearch',
+                          'password': '8isPo!@TWDuj9G8J',
+                          'database': 'vsearchlogDB', }
+
 
 def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as log:
-        print(req.form,
-              req.remote_addr,
-              req.user_agent,
-              res,
-              file=log,
-              sep='|')
+    with UserDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """INSERT INTO log 
+                (phrase, letters, ip, browser_string, results)
+                values 
+                (%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL, (req.form['phrase'],
+                              req.form['letters'],
+                              req.remote_addr,
+                              req.headers.get('User-Agent'),
+                              res, ))
 
 
 @app.route('/', methods=['GET'])
@@ -37,13 +46,12 @@ def do_search() -> 'html':
 
 @app.route('/viewlog', methods=['GET'])
 def view_the_log() -> 'html':
-    contents = []
-    with open('vsearch.log') as log:
-        for line in log:
-            contents.append([])
-            for item in line.split('|'):
-                contents[-1].append(escape(item))
-    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    with UserDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """SELECT phrase, letters, ip, browser_string, results 
+                FROM log"""
+        cursor.execute(_SQL)
+        contents = cursor.fetchall()
+    titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
     return render_template('viewlog.html',
                            the_title='View Log',
                            the_row_titles=titles,
